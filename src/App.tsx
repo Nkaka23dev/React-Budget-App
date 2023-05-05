cc708bdb5e8fbc48759648e130ff48952a41864f
Nkaka23dev <nkakaeric96@gmail.com>
2023-05-05 12:04:10 +0300

    refactor: delete app css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <h1 className="text-3xl text-center text-red-600 font-bold underline">
-      Hello world!
-    </h1>
+    <h1 className="text-3xl text-center text-red-600 font-bold underline">  
+     React Router start up
+      </h1>
   )
 }
